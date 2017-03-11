2d87544509770af619f700263bf1ce3cba4016b4
lillianwang16 <lillianwang16@gmail.com>
2017-03-11 11:31:52 -0800

    test $ operater

--- protos/test.cpp
+++ protos/test.cpp
@@ -8,16 +8,18 @@ int main() {
 int age ;
 age = 4 ;
 int name ;
-name = (3 % 4) ;
+name = (3 / 4) ;
 cout << age << ' ' << name << endl;
-
+int address ;
 Person person ;
 person.set_age(24) ;
 person.add_names(3) ;
 age = person.age() ;
 name = person.names(0) ;
+person.mutable_home()->set_address(4) ;
+address = person.mutable_home()->address()
 
-cout << age << ' ' << name << endl;
+cout << age << ' ' << name << ' ' << address << endl;
 return 0;
 }
 
